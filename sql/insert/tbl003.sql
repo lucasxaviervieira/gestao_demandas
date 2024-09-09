55bd145a935f0476c421f33291a5b547989bbd8c
lucasxaviervieira <lucasdavid.xavier@gmail.com>
2024-09-09 17:31:49 -0300

    feat: add new example of insert

--- sql/insert/tbl003.sql
+++ sql/insert/tbl003.sql
@@ -1,23 +1,32 @@
-INSERT INTO public."Demanda" 
-	(
-	codigo,
-	atividade_id,
-	localizacao_id,
-	sublocalidade_id,
-	tipo_id,
-	referencia_externa_id,
-	okr_id
-	)
-	VALUES
-	('ANA_AMB', '', '', '', '', '', '')
-	('APR_PRO', '', '', '', '', '', '')
-	('CON_PEN', '', '', '', '', '', '')
-	('CON_SER', '', '', '', '', '', '')
-	('DIV', '', '', '', '', '', '')
-	('MAT_CON', '', '', '', '', '', '')
-	('MAT_LIC', '', '', '', '', '', '')
-	('OFI', '', '', '', '', '', '')
-	('OKR', '', '', '', '', '', '')
-	('PAR_CON_AMB', '', '', '', '', '', '')
-	('REL_CON', '', '', '', '', '', '')
-	('VIS', '', '', '', '', '', '')
+DO $$
+DECLARE
+    atividade_id INTEGER;
+    localizacao_id INTEGER;
+    sublocalidade_id INTEGER;
+    tipo_id INTEGER;
+    -- referencia_externa_id INTEGER;
+    okr_id INTEGER;
+BEGIN
+	
+    SELECT id INTO atividade_id FROM public."Atividade" WHERE codigo = 'OKR';
+    SELECT id INTO localizacao_id FROM public."Localizacao" WHERE codigo = 'ALM';
+    SELECT id INTO sublocalidade_id FROM public."Sublocalidade" WHERE codigo = 'ADU';
+    SELECT id INTO tipo_id FROM public."Tipo" WHERE codigo = 'ALV_CON';
+    -- SELECT id INTO referencia_externa_id FROM public."Referencia_Externa" WHERE codigo = 'CGA';
+    SELECT id INTO okr_id FROM public."Obj_Res_Cha" WHERE codigo = '1_2024';
+
+    
+    INSERT INTO public."Demanda" 
+		(
+		atividade_id,
+		localizacao_id,
+		sublocalidade_id,
+		tipo_id,
+		referencia_externa_id,
+		okr_id
+		) VALUES
+        (atividade_id, localizacao_id, sublocalidade_id, tipo_id, null, null);
+
+    
+    RAISE NOTICE 'Tabela: Demanda; Inserção realizada com sucesso!';
+END $$;
