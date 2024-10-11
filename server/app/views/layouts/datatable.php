6b8751154fda0877092881ecfa5dcff078e60678
lucasxaviervieira <lucasdavid.xavier@gmail.com>
2024-10-11 14:32:22 -0300

    feat: ordering datatables by created date

--- server/app/views/layouts/datatable.php
+++ server/app/views/layouts/datatable.php
@@ -13,24 +13,27 @@
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     <script src="https://cdn.datatables.net/2.1.7/js/dataTables.js"></script>
     <script>
-        let screenHeight = screen.height;
-        let scrollY = screenHeight - screenHeight * 0.35
-        $(document).ready(function() {
-            new DataTable('#datatable', {
-                scrollX: true,
-                scrollY: scrollY,
-                info: false,
-                paging: false,
-                language: {
-                    search: "Pesquisar:",
-                    searchPlaceholder: "Digite para pesquisar...",
-                    zeroRecords: "Nenhum registro correspondente encontrado."
-                }
-            });
-            $('#column_filter').on('keyup', function() {
-                table.column(1).search(this.value).draw();
-            });
+    let screenHeight = screen.height;
+    let scrollY = screenHeight - screenHeight * 0.35
+    $(document).ready(function() {
+        new DataTable('#datatable', {
+            scrollX: true,
+            scrollY: scrollY,
+            aaSorting: [
+                []
+            ],
+            info: false,
+            paging: false,
+            language: {
+                search: "Pesquisar:",
+                searchPlaceholder: "Digite para pesquisar...",
+                zeroRecords: "Nenhum registro correspondente encontrado."
+            }
         });
+        $('#column_filter').on('keyup', function() {
+            table.column(1).search(this.value).draw();
+        });
+    });
     </script>
 
 </head>
